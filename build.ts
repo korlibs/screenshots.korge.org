589c21149abbc284d19bad2478a20d937030183b
soywiz <soywiz@gmail.com>
2024-02-21 06:37:57 +0100

    Remove github header to have more useful content on the projectsp age

--- build.ts
+++ build.ts
@@ -6,22 +6,27 @@ function timeout(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-async function screenshot(page: puppeteer.Page, filename: string, url: string) {
+async function screenshot(page: puppeteer.Page, filename: string, url: string, script: ((() => void) | undefined) = undefined) {
     console.log(`Taking screenshot for '${url}'`)
     await page.goto(url);
     await timeout(3000)
+    if (script) {
+        await page.evaluate(script)
+    }
     await page.screenshot({path: filename});
     console.log(`  -> done: ${filename}`)
 }
+
+declare const document: any
 
 (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({width: 1280, height: 1280})
-    await page.emulateMediaFeatures([
-        {name: 'prefers-color-scheme', value: 'dark'},
-    ]);
-    await screenshot(page, 'projects.png', 'https://github.com/orgs/korlibs/projects/19/views/23')
+    await page.emulateMediaFeatures([{name: 'prefers-color-scheme', value: 'dark'}]);
+    await screenshot(page, 'projects.png', 'https://github.com/orgs/korlibs/projects/19/views/23', () => {
+        document.querySelector('header')?.remove()
+    })
     await screenshot(page, 'root.png', 'https://korge.org/')
     await screenshot(page, 'docs.png', 'https://docs.korge.org/')
     await screenshot(page, 'blog.png', 'https://blog.korge.org/')
